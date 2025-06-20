3e858c9920523fd849ba2fffd32a81559f67127a
soyluy <ysssoylu@gmail.com>
2025-06-20 17:02:43 +0300

    Create authentication middleware

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -5,7 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { AppModule } from './root/app.module';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
